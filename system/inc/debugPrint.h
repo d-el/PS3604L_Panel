4ce1df15b2a5c4d2b6d7de55288c73359f13f8c2
Storozhenko Roman <del2.718@gmail.com>
2017-10-06 16:20:01 +0300

    Added net, debugPrint

---
+++ system/inc/debugPrint.h
@@ -0,0 +1,68 @@
+/*!****************************************************************************
+ * @file		debugPrint.h
+ * @author		d_el - Storozhenko Roman
+ * @version		V1.0
+ * @date		6 окт. 2017 г.
+ * @copyright	GNU Lesser General Public License v3
+ * @brief		--
+ */
+#ifndef debugPrint_H
+#define debugPrint_H
+
+/*!****************************************************************************
+ * Include
+ */
+
+/*!****************************************************************************
+ * Define
+ */
+
+/*!****************************************************************************
+ * Enumeration
+ */
+
+/*!****************************************************************************
+ * Typedef
+ */
+
+/*!****************************************************************************
+ * Exported variables
+ */
+
+/*!****************************************************************************
+ * Macro functions
+ */
+void println(const char *fmt, ...);
+void print(const char *fmt, ...);
+
+#ifdef NDEBUG           /* required by ANSI standard */
+#	define print(e) ((void)0)
+#else
+#	define print l_print
+#endif /* !NDEBUG */
+
+#ifdef NDEBUG           /* required by ANSI standard */
+#	define println(e) ((void)0)
+#else
+#	define println l_println
+#endif /* !NDEBUG */
+
+#ifdef NDEBUG           /* required by ANSI standard */
+#	define printdmsg(debug, message) ((void)0)
+#else
+#	define printdmsg(debug, message) \
+		do { \
+			if(debug){ \
+				print message; \
+			} \
+		}while(0);
+#endif /* !NDEBUG */
+
+/*!****************************************************************************
+ * Function declaration
+ */
+void l_print(const char *fmt, ...);
+void l_println(const char *fmt, ...);
+
+#endif //debugPrint_H
+/*************** LGPL ************** END OF FILE *********** D_EL ************/
---
+++ system/src/debugPrint.c
@@ -0,0 +1,74 @@
+/*!****************************************************************************
+ * @file		debugPrint.c
+ * @author		d_el - Storozhenko Roman
+ * @version		V1.0
+ * @date		06.10.2017
+ * @copyright	GNU Lesser General Public License v3
+ * @brief		--
+ */
+
+/*!****************************************************************************
+ * Include
+ */
+#include "stdint.h"
+#include "debugCore.h"
+#include "stdarg.h"
+#include "stdio.h"
+#include "string.h"
+#include "semihosting.h"
+
+/*!****************************************************************************
+ * MEMORY
+ */
+#define DEBUG_BUF_LEN	64
+static char g_buf[DEBUG_BUF_LEN];
+
+/*!****************************************************************************
+ * @brief    print debug message, printf format
+ */
+void l_print(const char *fmt, ...){
+	if(coreIsInDebugMode() == 0){
+		return;
+	}
+	va_list va;
+	va_start(va, fmt);
+	vsiprintf(g_buf, fmt, va);
+	va_end(va);
+	SH_SendString(g_buf);	//Send from semihosting
+}
+
+/*!****************************************************************************
+ * @brief    print debug message, printf format
+ */
+void l_println(const char *fmt, ...){
+	if(coreIsInDebugMode() == 0){
+		return;
+	}
+	va_list va;
+	va_start(va, fmt);
+	vsiprintf(g_buf, fmt, va);
+	va_end(va);
+	strcat(g_buf, "\n");
+	SH_SendString(g_buf);	//Send from semihosting
+}
+
+/*!****************************************************************************
+ * @brief writes count bytes from buffer into the standard output
+ * @param fd: file descriptor of file into which data is written
+ * @param buf: data to be written
+ * @param count: number of bytes
+ * @return returns the number of bytes actually written
+ */
+int _write (int fd, const void *buf, size_t count){
+	if(count > DEBUG_BUF_LEN + 1){
+		return -1;
+	}
+	memcpy(g_buf, buf, count);
+	g_buf[count] = 0;
+
+	SH_SendString(g_buf);
+	return count;
+}
+
+
+/*************** LGPL ************** END OF FILE *********** D_EL ************/
