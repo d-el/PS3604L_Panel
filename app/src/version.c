0600e9c462da6dfd972162ee847d22995630e226
Srorozhenko Roman <d2.718l@gmail.com>
2018-02-21 16:01:27 +0200

    Added makefile to .editorconfig

--- app/src/version.c
+++ app/src/version.c
@@ -8,10 +8,10 @@
 const uint32_t fwinfoMajor    = 2;
 const uint32_t fwinfoMinor    = 1;
 const uint32_t fwinfoRevision = 0;
-const uint32_t fwinfoBuild    = 2769;
+const uint32_t fwinfoBuild    = 2773;
 
-const char fwinfoVersion[] = {"2.1.0.2769"};
-const char fwinfoDate[] = {"2018-02-19"};
-const char fwinfoTime[] = {"07:03:37"};
+const char fwinfoVersion[] = {"2.1.0.2773"};
+const char fwinfoDate[] = {"2018-02-20"};
+const char fwinfoTime[] = {"22:27:29"};
 
 /***************** Copyright (C) Storozhenko Roman ******* END OF FILE *******/
