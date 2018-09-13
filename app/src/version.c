aafd30b27bc117cba4507d30eb88887aecf886a0
d-el <d2.718l@gmail.com>
2018-09-13 23:14:02 +0300

    Setting project

--- app/src/version.c
+++ app/src/version.c
@@ -8,10 +8,10 @@
 const uint32_t fwinfoMajor    = 2;
 const uint32_t fwinfoMinor    = 1;
 const uint32_t fwinfoRevision = 6;
-const uint32_t fwinfoBuild    = 4047;
+const uint32_t fwinfoBuild    = 4051;
 
-const char fwinfoVersion[] = {"2.1.5.4047"};
-const char fwinfoDate[] = {"2018-09-09"};
-const char fwinfoTime[] = {"20:27:16"};
+const char fwinfoVersion[] = {"2.1.6.4051"};
+const char fwinfoDate[] = {"2018-09-13"};
+const char fwinfoTime[] = {"20:12:47"};
 
 /******************************** END OF FILE ********************************/
