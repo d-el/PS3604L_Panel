75f4d276886167e47f7831252c80774d4ac83537
d-el <d2.718l@gmail.com>
2025-05-21 04:16:47 +0300

    Update script
    Signed-off-by: d-el <d2.718l@gmail.com>

--- scripts/gdb-serv.sh
+++ scripts/gdb-serv.sh
@@ -2,28 +2,32 @@
 
 function usage()
 {
-    cat << HEREDOC
-    Usage: $progname <openocd-stlink, openocd-jlink, openocd-mculink"
+	cat << HEREDOC
+	Usage: $progname <openocd-stlink, openocd-jlink, openocd-mculink"
 HEREDOC
 }
 
 if [ "$#" -lt 1 ]; then
-    usage
+	debugger=${DEBUGGER:-openocd-jlink}
+else
+	debugger=$1
 fi
 
 openocdcfg=${OPENOCDCFG:-$(dirname "$0")/openocd.cfg}
-echo "Use config: ${openocdcfg}"
 
+echo "Debugger: ${debugger}"
+echo "Config: ${openocdcfg}"
 echo "start" $1 "${@:2}"
-if [ "$1" == "openocd-stlink" ]; then
-    openocd -f interface/stlink-dap.cfg -c "transport select dapdirect_swd" -f ${openocdcfg} "${@:2}"
 
-elif [ "$1" == "openocd-jlink" ]; then
-    openocd -f interface/jlink.cfg -c "transport select swd" -f ${openocdcfg} "${@:2}"
+if [ ${debugger} == "openocd-stlink" ]; then
+	openocd -f interface/stlink-dap.cfg -c "transport select dapdirect_swd" -f ${openocdcfg} "${@:2}"
 
-elif [ "$1" == "openocd-mculink" ]; then
-    openocd -f interface/cmsis-dap.cfg -c "transport select swd" -f ${openocdcfg} "${@:2}"
+elif [ ${debugger} == "openocd-jlink" ]; then
+	openocd -f interface/jlink.cfg -c "transport select swd" -f ${openocdcfg} "${@:2}"
+
+elif [ ${debugger} == "openocd-mculink" ]; then
+	openocd -f interface/cmsis-dap.cfg -c "transport select swd" -f ${openocdcfg} "${@:2}"
 
 else
-    usage
+	usage
 fi
